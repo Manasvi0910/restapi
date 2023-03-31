756e5fe3681424262a1c0e46e5870c57b28b4067
Manasvi0910 <97155655+Manasvi0910@users.noreply.github.com>
2023-03-31 11:43:53 +0530

    Update api.js

--- api.js
+++ api.js
@@ -5,26 +5,43 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
+// Define a GET route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// Define a POST route for the '/api' URL
 app.post('/api', (req, res) => {
-    const data = req.body.data;
-    const userId = req.body.user_id;
-    const emailId = req.body.email_id;
-    const collegeRollNo = req.body.college_roll_no;
+  try {
+    // Extract data from request body
+    const { data, user_id, email_id, college_roll_no } = req.body;
+
+    // Filter even and odd numbers and uppercase alphabets
     const evenNumbers = data.filter(num => num % 2 === 0);
     const oddNumbers = data.filter(num => num % 2 !== 0);
     const alphabetList = data.filter(char => typeof char === 'string' && /[a-zA-Z]/.test(char)).map(char => char.toUpperCase());
+
+    // Construct response
     const response = {
-        status: 'success',
-        user_id: userId,
-        email_id: emailId,
-        college_roll_no: collegeRollNo,
-        even_numbers: evenNumbers,
-        odd_numbers: oddNumbers,
-        alphabet_list: alphabetList
+      status: 'success',
+      is_success: true,
+      user_id: `john_doe_${new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\//g, '')}`,
+      email_id,
+      college_roll_no,
+      even_numbers: evenNumbers,
+      odd_numbers: oddNumbers,
+      alphabet_list: alphabetList
     };
+
+    // Send response
     res.json(response);
+  } catch (error) {
+    // Handle exceptions gracefully
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error', is_success: false });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
